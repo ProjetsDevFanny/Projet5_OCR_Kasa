63628b39d205d0606121920e33e8a4729b44dad3
ProjetsDevFanny <contact.fanny.simon@gmail.com>
2025-10-08 12:29:35 +0200

    feat(HeroBannerHome, Home, Card): add HeroBannerHome, Home, Card components and their scss styles

---
+++ src/components/HeroBannerHome/HeroBannerHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './heroBannerHome.scss';
+
+function HeroBannerHome() {
+  return (
+    <section className="heroBanner">
+       <h1>Chez vous, partout et ailleurs</h1>
+    </section>
+  )
+}
+
+export default HeroBannerHome;
+  
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './home.scss'
+import HeroBannerHome from '../../components/HeroBannerHome/HeroBannerHome'
+import Card from '../../components/Card/Card'
 
 function Home() {
   return (
     <div className="home">
-     
+      <HeroBannerHome />
+      <div className="home__gallery">
+        <Card />
+      </div>
     </div>
   )
 }
